c5ed79386340313c352c88aed9e4726e05bb927b
Pawan paudel <paudelpwn@gmail.com>
2023-03-01 12:31:59 +0545

    Initial commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,8 @@
+
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      Developed By Pawan Paudel
+    </div>
+    <strong>&copy; <?php echo date('Y')?> - Point of Sale System </strong> - PHP Laravel || <a href="{{"https://github.com/pawan0ox"}}">My-github</a> ||
+  </footer>
+
